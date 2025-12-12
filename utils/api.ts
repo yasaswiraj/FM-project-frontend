dc8d8c7b8282cdaae105f7dcf99df5fc108e4043
yasaswiraj <yasaswirajmadari@gmail.com>
2025-12-11 20:12:21 -0500

    added env

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
